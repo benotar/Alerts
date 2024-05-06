a9a0f870fc2ea6a9e834b2c9b971f4decb892781
Ivan <zhmur.ivan@gmail.com>
2024-05-06 13:02:52 +0300

    chore(backend): improved the endpoint GetAlerts

--- src/Alerts.WebApp/Controllers/AlertsController.cs
+++ src/Alerts.WebApp/Controllers/AlertsController.cs
@@ -1,6 +1,7 @@
 ﻿using Alerts.Application.Alerts.Commands;
 using Alerts.Application.Interfaces.Services;
 using Microsoft.AspNetCore.Mvc;
+using static System.String;
 
 namespace Alerts.WebApp.Controllers;
 
@@ -16,13 +17,26 @@ public class AlertsController : Controller
     }
 
     [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetActiveAlerts()
     {
         var url = _alertsService.GetActiveAlertsUrl();
 
+        if (IsNullOrEmpty(url))
+        {
+            return BadRequest();
+        }
+        
         var getActiveAlerts = new GetActiveAlerts(url);
 
         var result = await getActiveAlerts.GetAlertsAsync();
+
+        if (result is null)
+        {
+            return NotFound();
+        }
         
         return Ok(result);
     }
