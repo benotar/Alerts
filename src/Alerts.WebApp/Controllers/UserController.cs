3f28f2deaa710d33595863b98aac18e73493641f
Ivan <zhmur.ivan@gmail.com>
2024-05-09 10:30:44 +0300

    chore(backend): Improved UserController endpoints

--- src/Alerts.WebApp/Controllers/UserController.cs
+++ src/Alerts.WebApp/Controllers/UserController.cs
@@ -15,12 +15,20 @@ public class UserController : Controller
     {
         _db = db;
     }
-
+    
+    [AllowAnonymous]
+    [Authorize(Roles = "User")]
     [HttpGet("GetUsers")]
     [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetUsers()
     {
+        if (!User.Identity.IsAuthenticated)
+        {
+            return Unauthorized("User is not authenticated!");
+        }
+        
         var users = await _db.Users.ToListAsync();
 
         if (users is null)
@@ -28,25 +36,36 @@ public class UserController : Controller
             return NotFound("No users found in the database!");
         }
         
-        
         return Ok(users);
     }
     
     [AllowAnonymous]
     [Authorize(Roles = "User")]
-    [HttpPut("AddRegion/{userId:long}")]
+    [HttpPut("AddRegion/{userId:long}/{userRegion}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> AddRegion(long userId ,string region)
+    public async Task<IActionResult> AddRegion(long userId ,string userRegion)
     {
+        if (!User.Identity.IsAuthenticated)
+        {
+            return Unauthorized("User is not authenticated!");
+        }
+        
         var user = await _db.Users.Where(u => u.Id == userId).FirstOrDefaultAsync();
 
         if (user is null)
         {
             return NotFound("User was not found in the database!");
         }
+
+        if (string.IsNullOrEmpty(userRegion))
+        {
+            return BadRequest("The region cannot be empty!");
+        }
         
-        user.Regions.Add(region);
+        user.Regions.Add(userRegion);
 
         await _db.SaveChangesAsync();
 
@@ -57,9 +76,16 @@ public class UserController : Controller
     [Authorize(Roles = "User")]
     [HttpDelete("DeleteRegion/{userId:long}/{userRegion}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> DeleteRegion(long userId ,string userRegion)
     {
+        if (!User.Identity.IsAuthenticated)
+        {
+            return Unauthorized("User is not authenticated!");
+        }
+        
         var user = await _db.Users.Where(u => u.Id == userId).FirstOrDefaultAsync();
 
         if (user is null)
@@ -67,6 +93,11 @@ public class UserController : Controller
             return NotFound("User was not found in the database!");
         }
 
+        if (string.IsNullOrEmpty(userRegion))
+        {
+            return BadRequest("The region cannot be empty!");
+        }
+        
         var region = user.Regions.FirstOrDefault(r => r == userRegion);
         
         if (string.IsNullOrEmpty(region))
