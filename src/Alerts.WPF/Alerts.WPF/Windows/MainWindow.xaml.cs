14ad9eb0dfba0df08abe33bba9e713eacecbd12f
Ivan <zhmur.ivan@gmail.com>
2024-05-09 14:23:54 +0300

    feat(WPF):Add Material Design lib

---
+++ src/Alerts.WPF/Alerts.WPF/Windows/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace Alerts.WPF.Windows;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
