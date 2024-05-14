4a047b7e470f56d178b1296bfc4ddb03575288e2
Ivan <zhmur.ivan@gmail.com>
2024-05-14 14:24:03 +0300

    Added unfinished logic to retrieve alarm information

--- src/Alerts.WPF/Controls/AlertsControl.xaml.cs
+++ src/Alerts.WPF/Controls/AlertsControl.xaml.cs
@@ -1,17 +1,24 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using Alerts.WPF.Hepler;
 
 namespace Alerts.WPF.Controls;
 
 public partial class AlertsControl : UserControl
 {
-    public AlertsControl()
+    private readonly AlertsModels.Alerts _alert;
+    public AlertsControl(AlertsModels.Alerts alert)
     {
         InitializeComponent();
+        
+        _alert = alert;
     }
 
     private void Load(object sender, RoutedEventArgs e)
     {
-        throw new NotImplementedException();
+        if (!AlertsHelper.IsActiveAlert(_alert, _alert.LocationTitle))
+        {
+            IsActiveAlertLabel.Content = "Не активна";
+        }
     }
 }
